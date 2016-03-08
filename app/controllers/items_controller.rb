2199b331f4bdfe88dc1e0273f7987a07a6cc24a2
Ted <ekeast328@gmail.com>
2016-03-07 19:37:14 -0500

    Checkpoint 7 Completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,13 +4,32 @@ class ItemsController < ApplicationController
 
   def create
     @item = current_user.items.new(item_params)
+    @new_item = Item.new
+    
     if @item.save
       flash[:notice] = "Activity was saved."
-      redirect_to user_path(@user)
     else
       flash[:notice] = "There was an error saving your activity. Please try again."
-      redirect_to user_path(@user)
     end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def destroy
+    @item = current_user.items.find(params[:id])
+    if @item.destroy
+      flash[:notice] = "Activity was deleted successfully."
+    else
+      flash.now[:alert] = "There was an error deleting the activity."
+    end
+  end
+
+  respond_to do |format|
+    format.html
+    format.js
   end
 
   private
